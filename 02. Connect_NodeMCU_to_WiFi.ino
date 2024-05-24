aa2fc6dfb0ecef624332244e6f34c99dcabd420d
TechWith_Deven <devendragawade3112@gmail.com>
2024-05-24 13:57:56 +0530

    Create 02. Connect_NodeMCU_to_WiFi.ino

---
+++ 02. Connect_NodeMCU_to_WiFi.ino
@@ -0,0 +1,17 @@
+#include <ESP8266WiFi.h>
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin("Mooazam", "mooazam123");
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print("..");
+    delay(200);
+  }
+  Serial.println();
+  Serial.println("NodeMCU is Connected!");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+}
